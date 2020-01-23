8c21160261aaecc7a4d26f293e69994f995dbe04
fmaurici <60138030+fmaurici@users.noreply.github.com>
2020-01-23 10:43:15 -0300

    Agrego Repositorios para Authors

---
+++ IRepositories/IAuthorRepository.cs
@@ -0,0 +1,11 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IRepositories
+{
+    public interface IAuthorRepository : IBaseRepository<Author>
+    {
+    }
+}
---
+++ Repositories/AuthorRepository.cs
@@ -0,0 +1,20 @@
+﻿using Database;
+using Entities;
+using IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repositories
+{
+    public class AuthorRepository : BaseRepository<Author>, IAuthorRepository
+    {
+        private readonly BookStoreContext _context;
+
+        public AuthorRepository(BookStoreContext context) : base(context)
+        {
+            _context = context;
+        }
+
+    }
+}
